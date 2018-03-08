e36cab449d5a32dc69bf4a0222111106f35ea210
dlewan <d.lewan2000@gmail.com>
2018-03-08 02:29:20 -0500

    Create cpio-wanted.el
    Initial code from local CVS.

---
+++ cpio-wanted.el
@@ -0,0 +1,70 @@
+;; -*- coding: utf-8 -*-
+;;; cpio-wanted.el --- Stuff cpio-mode wants from emacs lisp, but that isn't there.
+;	$Id: cpio-wanted.el,v 1.1.4.4 2018/03/08 06:10:15 doug Exp $	
+
+;; COPYRIGHT
+;; 
+;; Copyright © 2015, 2018 Douglas Lewan, d.lewan2000@gmail.com
+;; All rights reserved.
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Author: Douglas Lewan (d.lewan2000@gmail.com)
+;; Maintainer: -- " --
+;; Created: 2015 Dec 26
+;; Version: 0.01
+;; Keywords: wanted, not provided
+
+;;; Commentary:
+
+;; 
+;; Some things are just so generic that you expect emacs lisp
+;; to provide them.
+;; Unfortunately it doesn't provide some of them.
+;; This file contains such things that cpio-mode (or supporting code)
+;; wants, but can't find, in emacs lisp.
+;; 
+
+;;; Documentation:
+
+;;; Code:
+
+;;
+;; Dependencies
+;; 
+
+
+;; 
+;; Vars
+;; 
+
+
+;; 
+;; Library
+;; 
+
+
+;; 
+;; Commands
+;; 
+
+
+;; 
+;; Mode definition (IF APPROPRIATE)
+;; 
+
+
+(provide 'cpio-wanted)
+;;; cpio-wanted.el ends here
+
