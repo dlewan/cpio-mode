4d11b71fc4da100c0f3b2926ffd6e59e085b1806
dlewan <d.lewan2000@gmail.com>
2018-03-08 02:14:07 -0500

    Create cpio-crc.el
    Initial code from local CVS.

---
+++ cpio-crc.el
@@ -0,0 +1,57 @@
+;; -*- coding: utf-8 -*-
+;;; cpio-crc.el --- handle crc cpio entry header formats
+;	$Id: cpio-crc.el,v 1.3.2.1.2.1 2018/03/08 06:22:08 doug Exp $	
+
+;; COPYRIGHT
+;; 
+;; Copyright © 2016, 2017, 2018 Douglas Lewan, d.lewan2000@gmail.com.
+;; All rights reserved.
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+
+;; Author: Douglas Lewan (d.lewan2000@gmail.com)
+;; Maintainer: -- " --
+;; Created: 2015 Jan 03
+;; Version: 0.02
+;; Keywords: cpio, crc header
+
+;;; Commentary:
+
+;;; Documentation:
+
+;;; Code:
+
+;;
+;; Dependencies
+;; 
+
+
+;; 
+;; Vars
+;; 
+
+
+;; 
+;; Library
+;; 
+
+
+;; 
+;; Commands
+;; 
+
+
+
+;;; cpio-crc.el ends here.
