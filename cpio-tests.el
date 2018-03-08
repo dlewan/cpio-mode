70755a2b5945c2aba058590cfbd34ea60e8de96b
dlewan <d.lewan2000@gmail.com>
2018-03-08 02:27:56 -0500

    Create cpio-tests.el
    Initial code from local CVS.

---
+++ cpio-tests.el
@@ -0,0 +1,70 @@
+;; -*- coding: utf-8 -*-
+;;; cpio-tests.el --- tests for cpio-mode as a whole.
+;	$Id: cpio-tests.el,v 1.1.4.3 2018/03/08 06:10:14 doug Exp $	
+
+;; COPYRIGHT
+;; 
+;; Copyright © 2017, 2018 Douglas Lewan, d.lewan2000@gmail.com.
+;; All rights reserved.
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Author: Douglas Lewan (d.lewan2000@gmail.com)
+;; Maintainer: -- " --
+;; Created: 2017 Nov 28
+;; Version: 
+;; Keywords: 
+
+;;; Commentary:
+
+;; This file contains ERT tests for cpio-mode.
+;; Truly generic code has not yet been tested.
+;; Generic code for affiliated buffers is tested in cab-test.el.
+;; cpio-modes.el contains code for manipulating file mode information;
+;; it is tested in cpio-modes-test.el.
+
+
+;;; Documentation:
+
+;;; Code:
+
+;;
+;; Dependencies
+;; 
+
+
+;; 
+;; Vars
+;; 
+
+
+;; 
+;; Library
+;; 
+
+
+;; 
+;; Commands
+;; 
+
+
+;; 
+;; Mode definition (IF APPROPRIATE)
+;; 
+
+
+
+(provide 'cpio-tests)
+;;; cpio-tests.el ends here
+
