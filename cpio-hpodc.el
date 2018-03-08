2463890f294c48d54eb3980232e2749d5f929f6e
dlewan <d.lewan2000@gmail.com>
2018-03-08 02:21:49 -0500

    Create cpio-hpodc.el
    Initial code from local CVS.

---
+++ cpio-hpodc.el
@@ -0,0 +1,57 @@
+;; -*- coding: utf-8 -*-
+;;; cpio-hpodc.el --- handle hpodc cpio entry header formats
+;	$Id: cpio-hpodc.el,v 1.3.2.2 2018/03/08 06:10:13 doug Exp $	
+
+;; COPYRIGHT
+;; 
+;; Copyright © 2015, 2018 Douglas Lewan, d.lewan2000@gmail.com.
+;; All rights reserved.
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+
+;; Author: Douglas Lewan (d.lewan2000@gmail.com)
+;; Maintainer: -- " --
+;; Created: 2015 Jan 03
+;; Version: 0.01
+;; Keywords: cpio, HP portable character header
+
+;;; Commentary:
+
+;;; Documentation:
+
+;;; Code:
+
+;;
+;; Dependencies
+;; 
+
+
+;; 
+;; Vars
+;; 
+
+
+;; 
+;; Library
+;; 
+
+
+;; 
+;; Commands
+;; 
+
+
+(provide 'cpio-hpodc)
+;;; cpio-hpodc.el ends here.
