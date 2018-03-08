12877b14560c8ea4b4861ce849f66809a818e135
dlewan <d.lewan2000@gmail.com>
2018-03-08 02:28:39 -0500

    Create cpio-ustar.el
    Initial code from local CVS.

---
+++ cpio-ustar.el
@@ -0,0 +1,27 @@
+;; -*- coding: utf-8 -*-
+;; $Id: cpio-ustar.el,v 1.3.2.1 2018/03/08 06:10:15 doug Exp $
+
+;; COPYRIGHT
+;; 
+;; Copyright © 2015, 2018 Douglas Lewan, d.lewan2000@gmail.com.
+;; All rights reserved.
+;; 
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; 
+
+;;; Tar archives are already handled by tar-mode.
+
+(error "Detection of tar archives should invoke tar-mode.")
+
+;;; cpio-ustar.el ends here.
